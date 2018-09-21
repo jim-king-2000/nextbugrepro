ec46553460655236e64dffb383345657e1a7ac6d
Jim Jin <jim_king_2000@163.com>
2018-09-21 14:57:57 +0800

    add mobx

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-)
+import { action, observable } from 'mobx';
+import { Provider, inject, observer } from 'mobx-react';
+import React, { Component } from 'react';
 
-export default Index
+class Store {
+  @observable show = true;
+
+  @action toggleShow() {
+    console.log('toggle');
+    this.show = !this.show;
+  }
+}
+
+const store = new Store();
+
+@inject('store')
+@observer
+class TestMobX extends Component {
+  constructor() {
+    super();
+    this.onclick = this.onclick.bind(this);
+  }
+
+  onclick() {
+    console.log('onclick');
+    this.props.store.toggleShow();
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.onclick}>Click Me!</button>
+        <p>{this.props.store.show && 'Hello Next.js'}</p>
+      </div>
+    )
+  }
+}
+
+const Index = () => (
+  <Provider store={store}>
+    <TestMobX />
+  </Provider>
+);
+
+export default Index;

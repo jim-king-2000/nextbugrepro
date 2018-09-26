d9f410b1a2b7a80615ffe249c95024f26ba4af64
Jim Jin <jim_king_2000@163.com>
2018-09-26 09:45:39 +0800

    bug repro

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,9 @@
-import { action, observable } from 'mobx';
-import { Provider, inject, observer } from 'mobx-react';
-import React, { Component } from 'react';
-
-class Store {
-  @observable show = true;
-
-  @action toggleShow() {
-    console.log('toggle');
-    this.show = !this.show;
-  }
-}
-
-const store = new Store();
-
-@inject('store')
-@observer
-class TestMobX extends Component {
-  constructor() {
-    super();
-    this.onclick = this.onclick.bind(this);
-  }
-
-  onclick() {
-    console.log('onclick');
-    this.props.store.toggleShow();
-  }
-
-  render() {
-    return (
-      <div>
-        <button type="button" onClick={this.onclick}>Click Me!</button>
-        <p>{this.props.store.show && 'Hello Next.js'}</p>
-      </div>
-    )
-  }
-}
+import { ReactBingmaps } from 'react-bingmaps';
 
 const Index = () => (
-  <Provider store={store}>
-    <TestMobX />
-  </Provider>
-);
+  <div>
+    <p>Hello Next.js</p>
+  </div>
+)
 
-export default Index;
+export default Index

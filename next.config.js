c46f6561ca73db45b4dd4df61b80565cfeb8774f
Jim Jin <jim_king_2000@163.com>
2018-11-19 16:58:02 +0800

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
+if (typeof require !== "undefined") {
+  require.extensions[".less"] = () => {};
+  require.extensions[".css"] = (file) => {};
+ }
+ 
 const withCSS = require('@zeit/next-css')
-
-module.exports = withCSS({
-  webpack: function (config) {
-    config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          name: '[name].[ext]'
-        }
-      }
-    })
-    return config
-  }
-})
+module.exports = withCSS()
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import { ReactBingmaps } from 'react-bingmaps';
 
 const Index = () => (
   <div>
-    <p>Hello Next.js</p>
+    <ReactBingmaps 
+      bingmapKey = 'AhDjnlgDaR87L08Y3Uchu87Ky5vfvj8pkIQfLhGZ2yplreOxws6f3XCMHj0tMAR-'> 
+    </ReactBingmaps>
   </div>
 )
 

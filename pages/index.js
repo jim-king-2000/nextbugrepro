f33330fcb6f87e71fb0b84145244d8a9aa7911f3
Jim Jin <jim_king_2000@163.com>
2019-01-24 09:41:00 +0800

    bug repro

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,14 @@
 import React, { Component } from 'react';
-import Select from '@atlaskit/select';
-import { Field } from '@atlaskit/form';
+import { B } from 'react-module';
 
 export default class extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: { label: 'c', value: 'c' } };
   }
-
-  options = [
-    { label: 'a', value: 'a' },
-    { label: 'b', value: 'b' },
-    { label: 'c', value: 'c' }
-  ];
 
   render() {
     return (
-      <Field label='select' name='select'>
-        {({ fieldProps: { id, ...rest }}) => (
-          <Select
-          options={this.options}
-          inputId={id}
-          value={this.state.value}
-          {...rest}
-        />)}
-      </Field>
+      <B />
     );
   }
 }

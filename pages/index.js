9969cf6c958eb09233363a70bfe38e7bf216d63b
Jim <jim_king_2000@163.com>
2019-04-17 22:00:25 +0800

    refactory

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import Button from '@atlaskit/button';
-import Tooltip from '@atlaskit/tooltip';
 
 export default () => (
-  <Tooltip content="Hello World">
-    <Button>Hover Over Me</Button>
-  </Tooltip>
+  <div>测试</div>
 );

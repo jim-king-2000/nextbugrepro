8aed408a0e88a53b76d20774dfbf3b7433aa2a0b
Jim <jim_king_2000@163.com>
2020-02-19 20:30:06 +0800

    report next bug

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import { Accordion, AccordionPanel, Box, CheckBox, Text } from 'grommet';
-import { Grommet } from 'grommet';
-import { grommet } from "grommet/themes";
-import { generate } from "grommet/themes/base";
-import { deepMerge } from "grommet/utils";
-
-const theme = deepMerge(generate(18), grommet);
+import { Grommet, Form, FormField, TextInput, Button  } from 'grommet';
 
 export default () => (
-  <Grommet theme={theme}>
-    <Accordion>
-      <AccordionPanel label="Panel 1">
-        <Box gap="small" pad="medium" background="light-2">
-          <CheckBox label="abc" />
-          <CheckBox label="123" />
-        </Box>
-      </AccordionPanel>
-      <AccordionPanel label="Panel 2">
-        <Box pad="medium" background="light-2">
-          <Text>Two</Text>
-        </Box>
-      </AccordionPanel>
-    </Accordion>
+  <Grommet plain>
+    <Form onSubmit={({ value }) => console.log(value)}>
+      <FormField name='password' label='password' required>
+        <TextInput name='password' type='password' />
+      </FormField>
+      <Button primary type='submit' label='sign-in' />
+    </Form>
   </Grommet>
 );

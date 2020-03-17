6c782c8df82514cc8093392227ece076b193fb0f
Jim <jim_king_2000@163.com>
2020-03-17 21:50:49 +0800

    new bug

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,19 @@
-import { useState } from 'react';
-import { Grommet, Button, Form, Select } from 'grommet';
-import { Qr } from 'grommet-icons';
+import { Grommet, List, Menu } from 'grommet';
+import { More } from 'grommet-icons';
 
 export default () => {
-  const [a, setA] = useState();
   return (
     <Grommet plain>
-      <Form>
-        <Select
-          name='some_name'
-          options={['a', 'b', 'd']}
-          value={a}
-          onChange={({ option }) => setA(option)}
-        />
-        <Button
-          icon={<Qr />}
-          label='test'
-          onClick={() => setA('a')}
-        />
-      </Form>
+      <List
+        primaryKey="name"
+        data={[
+          { name: 'Alan', percent: 20 },
+          { name: 'Bryan', percent: 30 },
+          { name: 'Chris', percent: 40 },
+          { name: 'Eric', percent: 80 },
+        ]}
+        action={d => <Menu icon={<More />} hoverIndicator items={[{ label: "one" }]} />}
+      />
     </Grommet>
   );
 }
